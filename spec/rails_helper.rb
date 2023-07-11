b350bfe8e07703006bb677d72a0e541391ec5e9a
Lauren Golden <llgolden@gmail.com>
2023-07-10 21:51:30 -0600

    Completed all merchants

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,7 +36,7 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  config.include FactoryBot::Syntax::Methods
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -72,13 +72,4 @@ RSpec.configure do |config|
     config.configure_rspec_metadata!
     config.default_cassette_options = { re_record_interval: 7.days }
   end
-
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
-  end
-
-  config.include FactoryBot::Syntax::Methods
 end
