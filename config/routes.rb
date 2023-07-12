013351b959247e1183f25874e0ee9965d20a49f0
Lauren Golden <llgolden@gmail.com>
2023-07-11 19:11:02 -0600

    Complete 1a, setup 1b

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants do 
-        resources :items
+        resources :items, controller: "merchants/items"
       end
+      resources :items
     end
   end
-  get "/api/v1/merchants/:id/items", to: "merchants/items#index"
 end
---
+++ spec/requests/api/v1/item_request_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe "Items API" do
+  it "sends a list of all items" do
+    id = create(:merchant).id
+    create_list(:item, 5, merchant_id: id)
+
+    get "/api/v1/items"
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(items[:data].count).to eq(3)
+
+    items[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+    end
+  end
+
+  xit "can get one item by its id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data][:id]).to eq("#{id}")
+
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
+  end
+end
