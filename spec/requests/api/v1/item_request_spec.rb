838a5d3bbeedc7bd7c7848ed47a507f8f1d37973
Lauren Golden <llgolden@gmail.com>
2023-07-11 19:15:23 -0600

    US 1b

--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -11,7 +11,7 @@ describe "Items API" do
 
     expect(response).to be_successful
 
-    expect(items[:data].count).to eq(3)
+    expect(items[:data].count).to eq(5)
 
     items[:data].each do |item|
       expect(item).to have_key(:id)
@@ -19,6 +19,15 @@ describe "Items API" do
 
       expect(item[:attributes]).to have_key(:name)
       expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+      
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to eq(id)
     end
   end
 
